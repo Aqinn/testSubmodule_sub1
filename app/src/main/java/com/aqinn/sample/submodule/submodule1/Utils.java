ba46fec596d4b0d8706e8534751120a4e9b92ea4
allanzhong <allanzhong@tencent.com>
2023-03-28 17:24:19 +0800

    test sub1 add

--- app/src/main/java/com/aqinn/sample/submodule/submodule1/Utils.java
+++ app/src/main/java/com/aqinn/sample/submodule/submodule1/Utils.java
@@ -7,6 +7,7 @@ public class Utils {
 
     public static void sayHi(Context context) {
         // test: root repo add
+        // test: sub1 repo add
         Toast.makeText(context, "Hi, here is sub module 1.", Toast.LENGTH_LONG).show();
     }
 
